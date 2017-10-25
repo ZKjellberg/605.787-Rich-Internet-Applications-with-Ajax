54022df3917fd065a35e67bc8f024ad6900a4f31
Zachary Kjellberg <zach.kjellberg@gmail.com>
2017-10-25 02:22:21 +0000

    Bootstrap themed results. Migrated name of results

--- module8-solution/js/app.js
+++ module8-solution/js/app.js
@@ -11,7 +11,7 @@
 
   function ShoppingListDirective() {
     var ddo = {
-      templateUrl: 'shoppingList.html',
+      templateUrl: 'menuList.html',
       scope: {
         items: '<',
         myTitle: '@title',
