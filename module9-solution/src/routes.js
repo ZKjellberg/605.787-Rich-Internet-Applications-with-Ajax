fa06274bf76b2b8b0f2f51b589fee0340cf32a98
Zachary Kjellberg <zach.kjellberg@gmail.com>
2017-11-05 18:55:10 +0000

    Migrated Lecture39 progress as Module9

--- module9-solution/src/routes.js
+++ module9-solution/src/routes.js
@@ -1,1 +1,50 @@
-// TODO
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuapp/templates/home.template.html'
+  })
+
+  // Premade list page
+  .state('mainList', {
+    url: '/categories',
+    templateUrl: 'src/menuapp/templates/main-shoppinglist.template.html',
+    controller: 'MainShoppingListController as mainList',
+    resolve: {
+      items: ['ShoppingListService', function (ShoppingListService) {
+        return ShoppingListService.getItems();
+      }]
+    }
+  })
+
+  .state('itemDetail', {
+    url: '/items/{itemId}',
+    templateUrl: 'src/menuapp/templates/item-detail.template.html',
+    controller: 'ItemDetailController as itemDetail',
+    resolve: {
+      item: ['$stateParams', 'ShoppingListService',
+            function ($stateParams, ShoppingListService) {
+              return ShoppingListService.getItems()
+                .then(function (items) {
+                  return items[$stateParams.itemId];
+                });
+            }]
+    }
+  });
+}
+
+})();
