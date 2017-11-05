07b3bd6370cf3de070c12b3f702d3bde43a7bd03
Zachary Kjellberg <zach.kjellberg@gmail.com>
2017-11-05 22:14:25 +0000

    Prep work for introducing REST call

--- module9-solution/src/menuapp/menudata.service.js
+++ module9-solution/src/menuapp/menudata.service.js
@@ -14,17 +14,14 @@ function MenuDataService($q, $timeout) {
   // Pre-populate a no cookie list
   items.push({
     name: "Sugar",
-    quantity: "2 bags",
     description: "Sugar used for baking delicious umm... baked goods."
   });
   items.push({
     name: "flour",
-    quantity: "1 bags",
     description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
   });
   items.push({
     name: "Chocolate Chips",
-    quantity: "3 bags",
     description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
   });
 
